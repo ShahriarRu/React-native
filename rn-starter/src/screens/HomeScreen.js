d8bdab276b437b95a3c3f2db9c57fd0ecd5d2b41
Shahriar Alif <shahriarcse26@gmail.com>
2022-01-29 12:57:22 +0600

    navigation functionalities added

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
+import { Button, TouchableOpacity, View } from "react-native-web";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi There!</Text>;
+const HomeScreen = ({ navigation }) => {
+  console.log();
+  return (
+    <View>
+      <Text style={styles.text}>Hi There!</Text>
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to components"
+      />
+      <Button onPress={() => navigation.navigate("List")} title="Go to List" />
+      <TouchableOpacity onPress={() => navigation.navigate("List")}>
+        <Text>This is List</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
