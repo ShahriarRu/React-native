ded5e841c61870a2e57533d2ac6f2033cb0a706e
Shahriar Alif <shahriarcse26@gmail.com>
2022-02-17 18:43:02 +0600

    context added

--- blog/App.js
+++ blog/App.js
@@ -1,7 +1,9 @@
+import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
-import IndexScreen from "./screens/IndexScreen";
+import IndexScreen from "./src/screens/IndexScreen";
+import { BlogProvider } from "./src/context/BlogContext";
 
 const navigator = createStackNavigator(
   {
@@ -15,4 +17,12 @@ const navigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
